f9b88b79b2ac52757044c953ebcfb7999a1c5fb0
Nicolle Gonzalez <nicolle.ngonzalez@gmail.com>
2019-08-08 10:00:08 -0400

    deploying

--- server/app.js
+++ server/app.js
@@ -98,7 +98,7 @@ app.use((req, res, next) => {
     } else next();
   });
   
-  const root = path.join(__dirname + "/../client/build/index.html");
+  const root = path.join(__dirname + "/../client/build/");
   app.use(express.static(root));
 
 module.exports = app;
