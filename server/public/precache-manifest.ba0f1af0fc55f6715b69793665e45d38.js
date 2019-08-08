5751d61a19849206fe1d51e442f12d748dbe964d
Nicolle Gonzalez <nicolle.ngonzalez@gmail.com>
2019-08-08 10:08:47 -0400

    deploying

---
+++ server/public/precache-manifest.ba0f1af0fc55f6715b69793665e45d38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374a9c56bd1dde9d024233e8af1b59eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "120966993c2a98dc37f8",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e903131d8f929c1e1df3",
+    "url": "/static/css/main.d35d5234.chunk.css"
+  },
+  {
+    "revision": "120966993c2a98dc37f8",
+    "url": "/static/js/2.a516f4bc.chunk.js"
+  },
+  {
+    "revision": "e903131d8f929c1e1df3",
+    "url": "/static/js/main.31cdb27c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ server/public/precache-manifest.f3bc4a5e97b3d54e143d2562e44ce85c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ecaa3580181db722c9c5d67c1a9943",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aabf24db595c6297265",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "28fffd26506c66fb17b7",
+    "url": "/static/css/main.d35d5234.chunk.css"
+  },
+  {
+    "revision": "2aabf24db595c6297265",
+    "url": "/static/js/2.8a129628.chunk.js"
+  },
+  {
+    "revision": "28fffd26506c66fb17b7",
+    "url": "/static/js/main.f5676aac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
